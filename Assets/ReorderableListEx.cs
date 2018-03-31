13221c39adfa980198741ed5bc264bd355a36258
capra314cabra <capra314cabra@gmail.com>
2018-03-31 20:05:14 +0900

    First stable version !

---
+++ Assets/ReorderableListEx.cs
@@ -0,0 +1,82 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace CPRUnitySystem
+{
+    public class EasyReorderableList<T>
+    {
+        private ReorderableList reorderableList;
+        private T t_default;
+        private ElementCallback<T> elementDrawCallback;
+
+        public List<T> Data { get; private set; }
+        public event OnListChangedEventHandler<T> OnListChanged;
+
+        public EasyReorderableList(string title, List<T> data, ElementCallback<T> drawCallback, T init = default(T))
+        {
+            Data = data;
+            t_default = init;
+            reorderableList = new ReorderableList(data, typeof(T));
+
+            reorderableList.drawHeaderCallback += (rect) =>
+            {
+                EditorGUI.LabelField(rect, title);
+            };
+
+            reorderableList.drawElementCallback += (rect, index, isActive, isFocused) =>
+            {
+                Data[index] = elementDrawCallback(rect, Data[index], isActive, isFocused);
+            };
+            elementDrawCallback = drawCallback;
+
+            reorderableList.onAddCallback += OnAdd;
+            reorderableList.onRemoveCallback += OnRemove;
+            reorderableList.onChangedCallback += OnChangedCallback;
+        }
+
+        public void DoList(Rect rect)
+        {
+            reorderableList.DoList(rect);
+        }
+
+        public void DoLayoutList()
+        {
+            reorderableList.DoLayoutList();
+        }
+
+        private void OnAdd(ReorderableList list)
+        {
+            Data.Add(t_default);
+        }
+
+        private void OnRemove(ReorderableList list)
+        {
+            var remove = Data.Count;
+            Data.RemoveAt(remove - 1);
+        }
+
+        private void OnChangedCallback(ReorderableList list)
+        {
+            if (OnListChanged != null)
+                OnListChanged(Data);
+        }
+    }
+
+    public delegate void OnListChangedEventHandler<T>(List<T> list);
+
+    public delegate T ElementCallback<T>(Rect rect, T data, bool isActive, bool isFocused);
+
+    [System.Obsolete("This interface is not used.")]
+    public interface IReorderableSetting
+    {
+        
+    }
+}
+
+#endif
---
+++ Assets/TestScriptable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using CPRUnitySystem;
+using System.Linq;
+
+[CreateAssetMenu]
+public class TestScriptable : ScriptableObject
+{
+    public List<int> i = new List<int>();
+    
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TestScriptable))]
+    public class TestEx : Editor
+    {
+        private EasyReorderableList<int> easyList;
+
+        public override void OnInspectorGUI()
+        {
+            var Target = target as TestScriptable;
+            if(easyList == null) easyList = new EasyReorderableList<int>("i", Target.i, Draw, 10);
+            easyList.DoLayoutList();
+            EditorUtility.SetDirty(Target);
+        }
+
+        public int Draw(Rect rect, int data, bool isActive, bool isFocused)
+        {
+            return EditorGUI.IntField(rect, "int", data);
+        }
+    }
+}
