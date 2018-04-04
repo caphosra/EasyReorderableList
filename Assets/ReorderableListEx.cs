7daf679dac49406da42df073fc0344370442a0cc
capra314cabra <capra314cabra@gmail.com>
2018-04-04 14:10:26 +0900

    Implement System.IDisposable

--- Assets/ReorderableListEx.cs
+++ Assets/ReorderableListEx.cs
@@ -9,15 +9,24 @@ using UnityEditorInternal;
 
 namespace CPRUnitySystem
 {
-    public class EasyReorderableList<T>
+    public class EasyReorderableList<T> : System.IDisposable
     {
         private ReorderableList reorderableList;
         private T t_default;
-        private ElementCallback<T> elementDrawCallback;
 
+        /// <summary>
+        /// List value
+        /// </summary>
         public List<T> Data { get; private set; }
+
+        /// <summary>
+        /// Event callback function when List value changed
+        /// </summary>
         public event OnListChangedEventHandler<T> OnListChanged;
 
+        /// <summary>
+        /// Create EasyReorderableList instance
+        /// </summary>
         public EasyReorderableList(string title, List<T> data, ElementCallback<T> drawCallback, T init = default(T))
         {
             Data = data;
@@ -31,20 +40,49 @@ namespace CPRUnitySystem
 
             reorderableList.drawElementCallback += (rect, index, isActive, isFocused) =>
             {
-                Data[index] = elementDrawCallback(rect, Data[index], isActive, isFocused);
+                Data[index] = drawCallback(rect, Data[index], isActive, isFocused);
             };
-            elementDrawCallback = drawCallback;
 
             reorderableList.onAddCallback += OnAdd;
             reorderableList.onRemoveCallback += OnRemove;
             reorderableList.onChangedCallback += OnChangedCallback;
         }
 
+        /// <summary>
+        /// Create EasyReorderableList instance
+        /// </summary>
+        public EasyReorderableList(string title, List<T> data, ElementCallbackWithIndex<T> drawCallback, T init = default(T))
+        {
+            Data = data;
+            t_default = init;
+            reorderableList = new ReorderableList(data, typeof(T));
+
+            reorderableList.drawHeaderCallback += (rect) =>
+            {
+                EditorGUI.LabelField(rect, title);
+            };
+
+            reorderableList.drawElementCallback += (rect, index, isActive, isFocused) =>
+            {
+                Data[index] = drawCallback(rect, index, Data[index], isActive, isFocused);
+            };
+
+            reorderableList.onAddCallback += OnAdd;
+            reorderableList.onRemoveCallback += OnRemove;
+            reorderableList.onChangedCallback += OnChangedCallback;
+        }
+
+        /// <summary>
+        /// Draw EasyReorderableList
+        /// </summary>
         public void DoList(Rect rect)
         {
             reorderableList.DoList(rect);
         }
 
+        /// <summary>
+        /// Draw EasyReorderableList (auto layout)
+        /// </summary>
         public void DoLayoutList()
         {
             reorderableList.DoLayoutList();
@@ -66,11 +104,33 @@ namespace CPRUnitySystem
             if (OnListChanged != null)
                 OnListChanged(Data);
         }
+
+        /// <summary>
+        /// Dispose this
+        /// </summary>
+        public void Dispose()
+        {
+            reorderableList = null;
+            t_default = default(T);
+            Data = null;
+            OnListChanged = null;
+        }
     }
 
+    /// <summary>
+    /// Event handler when List value changed
+    /// </summary>
     public delegate void OnListChangedEventHandler<T>(List<T> list);
 
+    /// <summary>
+    /// Drawing function
+    /// </summary>
     public delegate T ElementCallback<T>(Rect rect, T data, bool isActive, bool isFocused);
+
+    /// <summary>
+    /// Drawing function
+    /// </summary>
+    public delegate T ElementCallbackWithIndex<T>(Rect rect, int index, T data, bool isActive, bool isFocused);
 
     [System.Obsolete("This interface is not used.")]
     public interface IReorderableSetting
